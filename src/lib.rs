8e532d7ff205528d2eb21eb85326a5e683c52c67
Sc46 <sc46@qq.com>
2025-10-08 11:14:23 +0800

    v0.1

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,19 @@ use ostd::prelude::*;
 
 #[ostd::main]
 fn kernel_main() {
+    println!();
+    println!();
     println!("Hello world from guest kernel!");
+    println!("《大风歌》  刘邦");
+    println!("大风起兮云飞扬");
+    println!("威加海内兮归故乡");
+    println!("安得猛士兮守四方");
+    println!();
+    println!();
 }
 
 #[cfg(test)]
 mod unit_tests{
     use super::*;
 
-    #[ktest]
-    fn simple_test(){
-        assert_eq!(6*14,84);
-    }
 }
